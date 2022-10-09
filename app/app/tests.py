029574743391c517586d5ea556705c478f657001
Aditya Mathur <89768498+adii9@users.noreply.github.com>
2022-10-09 21:35:46 +0530

    Cleared Linting Issues in Test.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,12 +10,11 @@ class CalcTests(SimpleTestCase):
     
     def test_add_numbers(self):
 
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
 
         res = calc.sub(10, 15)
         self.assertEqual(res, 5)
-
 
